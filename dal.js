1dfdf67b52cfff4e086c9192370ff878ab79ec06
Benny Shtemer <75434503+benny779@users.noreply.github.com>
2023-07-18 22:54:41 +0300

    refactor

---
+++ dal.js
@@ -0,0 +1,63 @@
+const fs = require('fs');
+
+const dbFile = './inventory_list.json';
+
+const readAllProducts = (includeDeleted = false) => {
+  if (!fs.existsSync(dbFile)) return [];
+
+  const listStr = fs.readFileSync(dbFile, { encoding: 'utf8' });
+  const list = JSON.parse(listStr);
+  const listToSend = includeDeleted ? list : list.filter((p) => !p.isDeleted);
+  return listToSend;
+};
+
+const updateDB = (str) => {
+  try {
+    fs.writeFileSync(dbFile, str, 'utf8');
+  } catch {
+    return false;
+  }
+
+  return true;
+};
+
+const addProduct = (product) => {
+  let list = readAllProducts();
+  list.push(product);
+  return updateDB(JSON.stringify(list));
+};
+
+const getProductById = (uid) => {
+  const list = readAllProducts();
+  const product = list.find((p) => p.uid === uid);
+
+  return product;
+};
+
+const updateProductById = (product) => {
+  const list = readAllProducts(true);
+  const productIndex = list.findIndex((p) => p.uid == product.uid);
+
+  if (productIndex === -1) return false;
+
+  list[productIndex] = { ...list[productIndex], ...product };
+  return updateDB(JSON.stringify(list));
+};
+
+const deleteProductById = (uid) => {
+  const existingProduct = getProductById(uid);
+
+  if (!existingProduct) return 404;
+
+  existingProduct.isDeleted = true;
+  return updateProductById(existingProduct);
+};
+
+const exportModules = {
+  readAllProducts,
+  addProduct,
+  getProductById,
+  updateProductById,
+  deleteProductById,
+};
+module.exports = exportModules;
--- index.js
+++ index.js
@@ -2,93 +2,71 @@ const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const { v4: uuidv4 } = require('uuid');
+const {
+  readAllProducts,
+  addProduct,
+  getProductById,
+  updateProductById,
+  deleteProductById,
+} = require('./dal');
 const app = express();
 const port = 3000;
 
+/*FUNCTIONS*/
+const isTrue = (value) => (value + '').toLowerCase() === 'true';
+
 app.use(bodyParser.json());
 
-app.post('/products/create/', function (req, res) {
-    const newProduct = { uid: uuidv4(), ...req.body };
-    let list = [];
-  
-    if (fs.existsSync('inventory_list.json')) {
-      list = require('./inventory_list.json');
-    }
-  
-    list.push(newProduct);
-  
-    fs.writeFileSync('inventory_list.json', JSON.stringify(list, null, 2), (error) => {
-      if (error) console.log('error');
-      else console.log('successfuly');
-    });
-    const length = list.length
-    console.log(length);
-    res.json(newProduct);
-  });
-  
-  app.get('/products/', function (req, res) {
-    const listAll = require('./inventory_list.json');
-    res.json(listAll);
-  });
-  
-  app.get('/products/:id', function (req, res) {
-    const { id } = req.params;
-    const listAll = require('./inventory_list.json');
-    const currentProduct = listAll.find((product) => id == product.id);
-    if (currentProduct) res.json(currentProduct);
-    else res.status(404).send();
-  });
-  
-  app.put('/products/update/:id', function (req, res) {
-    const { id } = req.params;
-    const productDetails = req.body;
-  
-    const listAll = require('./inventory_list.json');
-    const currentProductIndex = listAll.findIndex((product) => id == product.id);
-    if (currentProductIndex == -1) {
-      res.status(404).send();
-    } else {
-      // update list
-      listAll[currentProductIndex] = { ...listAll[currentProductIndex], ...productDetails };
-      //save to file
-      fs.writeFileSync('inventory_list.json', JSON.stringify(listAll, null, 2), (error) => {
-        if (error) console.log('error');
-        else console.log('successfuly');
-      });
-      // return
-      res.send();
-    }
-  });
-  
-  app.post('/products/delete/:id', function (req, res) {
-    const { id } = req.params;
-    const listAll = require('./inventory_list.json');
-    const currentProductIndex = listAll.findIndex((product) => id == product.id);
-    if (currentProductIndex == -1) console.log('error');
-    else {
-      const productToArchives = listAll[currentProductIndex];
-      const updatedList = listAll.filter((product) => id != product.id);
-      fs.writeFileSync('inventory_list.json', JSON.stringify(updatedList, null, 2), (error) => {
-        if (error) console.log('error');
-        else console.log('succesfuly');
-      });
-  
-      let archivesList = [];
-  
-      if (fs.existsSync('archives_inventory_list.json')) {
-        archivesList = require('./archives_inventory_list.json');
-      }
-      archivesList.push(productToArchives);
-  
-      fs.writeFileSync('archives_inventory_list.json', JSON.stringify(archivesList, null, 2), (error) => {
-        if (error) console.log('error');
-        else console.log('succesfuly');
-      });
-      res.send();
-      const length = updatedList.length
-    console.log(length);
-    }
-    
-  });
-  
-  app.listen(port, () => console.log('server starts'));
+app.post('/products/', function (req, res) {
+  const newProduct = { uid: uuidv4(), ...req.body };
+
+  const productAdded = addProduct(newProduct);
+
+  if (productAdded) res.status(201).json(newProduct);
+  else res.status(400).json('Create product failed');
+});
+
+app.get('/products/', function (req, res) {
+  const { includeDeleted } = req.query;
+
+  const listAll = readAllProducts(isTrue(includeDeleted));
+
+  if (Array.isArray(listAll)) res.json(listAll);
+  else res.status(400).json('Read products failed');
+});
+
+app.get('/products/:uid', function (req, res) {
+  const { uid } = req.params;
+  const product = getProductById(uid);
+
+  if (product) res.json(product);
+  else res.status(404).send();
+});
+
+app.put('/products/:uid', function (req, res) {
+  const { uid } = req.params;
+  const product = req.body;
+
+  if (uid !== product.uid) {
+    res.status(409).json('uid error');
+    return;
+  }
+
+  if (updateProductById(product)) res.status(204).send();
+  else res.status(400).json('Update product failed');
+});
+
+app.delete('/products/:uid', function (req, res) {
+  const { uid } = req.params;
+
+  const result = deleteProductById(uid);
+
+  if (result === 404) {
+    res.status(404).send();
+    return;
+  } else {
+    res.status(204).send();
+  }
+});
+
+app.listen(port, () => console.log(`server is listening on http://localhost:${port}/`));
